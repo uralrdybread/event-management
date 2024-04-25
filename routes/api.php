599e4c3ce8976553275bc0b5ea7d31d5c2665282
Douglas White <douglas.rw@outlook.com>
2024-04-25 17:14:09 -0600

    first commit

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\EventController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::apiResource('events', EventController::class);
+Route::apiResource('events.attendees', AttendeeController::class)
+->scoped(['attendee' => 'event']);
