342dadf0f14aa677e9e47796b8a486463ef17304
Douglas White <douglas.rw@outlook.com>
2024-04-25 18:35:34 -0600

    Created the factory and seeders for the REST API

--- database/migrations/2024_04_25_224424_create_attendees_table.php
+++ database/migrations/2024_04_25_224424_create_attendees_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\User;
+use App\Models\Event;
 
 return new class extends Migration
 {
---
+++ database/seeders/EventSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class EventSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = User::all();
+
+        for ($i = 0; $i < 200; $i++) {
+            $user = $users->random();
+            \App\Models\Event::factory()->create([
+                'user_id' => $user->id
+            ]);
+        }
+    }
+}
